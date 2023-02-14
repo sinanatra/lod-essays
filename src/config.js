eac43eea42d91014b375c6d13604b63ad6e99a70
Giacomo Nanni <gn.nanni@gmail.com>
2023-02-14 17:42:55 +0100

    chore: token can't start with github

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const Api = "https://uclab.fh-potsdam.de/refa/api";
-export const Files = ["Sammlungsgeschichte-der-Lipperheideschen","The-5-senses"];
+// export const Files = ["Sammlungsgeschichte-der-Lipperheideschen","The-5-senses"];
+
+export const Files = ["test"];
